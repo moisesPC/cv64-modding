1d09baa46ea5803d082c15b7aa4ae97ba3b26e43
moisesPC <moisesapc64@gmail.com>
2024-10-26 16:37:10 +0100

    Actually fill static_segment.c

---
+++ include/mod.h
@@ -0,0 +1,13 @@
+/** 
+ * This header file contains content that is meant to be used specifically for mods,
+ * content that doesn't have a specific header.
+ */
+
+#ifndef MOD_H
+#define MOD_H
+
+#include "cv64.h"
+
+s32 insideGameplay();
+
+#endif
--- src/static/static_segment.c
+++ src/static/static_segment.c
@@ -1,10 +1,42 @@
-#include "item.h"
+#include "mod.h"
 
-u32 data_var = 10;
-const u32 rodata_var = 3;
-u32 bss_var;
+#include "objects/player/player.h"
+#include "objects/engine/GameStateMgr.h"
+#include "system_work.h"
 
-void static_main() {
-    data_var += ITEM_MATERIAL_DL * rodata_var;
-    bss_var  *= ITEM_MATERIAL_DL;
+/**
+ * Returns `TRUE` if all gameplay state subsystems have been initialized.
+ * 
+ * This is a safe way of checking if gameplay has started before accessing
+ * any of its elements.
+ */
+s32 insideGameplay() {
+    return ((ptr_PlayerData != NULL) && (sys.map_is_setup) && (ptr_GameStateMgr->current_game_state == GAMESTATE_GAMEPLAY));
+}
+
+/**
+ * Called once, right after the custom segment is loaded during bootup
+ * Extra initialization code can go here
+ */
+void hook_customSegmentInit() {
+    /* Your initialization code goes here */
+
+    /**
+     * Optional: Initialize the crash screen thread to be able
+     * to see debug output to the console when the game crashes
+     */
+    crashScreen_Init();
+}
+
+/**
+ * Called every frame at 60FPS
+ */
+void hook_customSegmentLoop() {
+    /* Your loop code goes here */
+
+    /**
+     * The code we replaced with our hook originally calls `controller_readData`
+     * Make the call here at the very end to resume normal execution
+     */
+    controller_readData();
 }
